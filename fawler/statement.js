f7ca1b8238a7282e2e2d445486507ce5615f3d9c
eclipse2ant <eclipse2ant@gmail.com>
2022-03-14 11:46:40 +0900

    plays variable

--- fawler/statement.js
+++ fawler/statement.js
@@ -1,38 +1,38 @@
-const invoices = require('./invoices.json');
-const plays = require('./plays.json');
+//const invoices = require('./invoices.json');
+//const plays = require('./plays.json');
 
-function statement (invoice) {
+function statement (invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
   // print line for this order
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf,plays).name}: ${usd(amountFor(perf, plays)/100)} (${perf.audience} seats)\n`;
   }
 
-  result += `Amount owed is ${usd(totalAmount(invoice)/100)}\n`;
-  result += `You earned ${totalvolumeCredits(invoice)} credits\n`;
+  result += `Amount owed is ${usd(totalAmount(invoice,plays)/100)}\n`;
+  result += `You earned ${totalvolumeCredits(invoice,plays)} credits\n`;
   return result;
 }
-function totalAmount(invoice) {
+function totalAmount(invoice, plays) {
   let result = 0;
   for (let perf of invoice.performances) {
-    result += amountFor(perf);
+    result += amountFor(perf, plays);
   }
   return result;
 }
 
-function totalvolumeCredits(invoice) {
+function totalvolumeCredits(invoice, plays) {
   let volumeCredits=0;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
+    volumeCredits += volumeCreditsFor(perf, plays);
   }
   return volumeCredits;
 }
 
-function volumeCreditsFor(aPerformance) {
+function volumeCreditsFor(aPerformance, plays) {
   let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if ("comedy" === playFor(aPerformance, plays).type) result += Math.floor(aPerformance.audience / 5);
     return result;
 }
 
@@ -42,9 +42,9 @@ function usd(aNumber) {
                         minimumFractionDigits: 2 }).format(aNumber);
 }
 
-function amountFor(aPerformance) {
+function amountFor(aPerformance, plays) {
   let result= 0;
-  switch (playFor(aPerformance).type) {
+  switch (playFor(aPerformance,plays).type) {
   case "tragedy":
     result = 40000;
     if (aPerformance.audience > 30) {
@@ -59,17 +59,17 @@ function amountFor(aPerformance) {
     result += 300 * aPerformance.audience;
     break;
   default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+      throw new Error(`unknown type: ${playFor(aPerformance, plays).type}`);
   }
   return result;
 }
 
-function playFor(aPerformance) {
+function playFor(aPerformance, plays) {
   return plays[aPerformance.playID];
 }
 
 module.exports = 
- {statement, amountFor, volumeCreditsFor, usd, totalvolumeCredits,
+ {statement, playFor, amountFor, volumeCreditsFor, usd, totalvolumeCredits,
   totalAmount
 };
 //module.exports.statement=statement;
@@ -83,4 +83,8 @@ console.log(plays["as-like"]);
 console.log(usd(-0.005));
 console.log(totalvolumeCredits(invoices[0]));
 console.log(totalAmount(invoices[0]));
+*/
+/*const invoices = require('./invoices.json');
+const plays = require('./plays.json');
+console.log(playFor(invoices[0].performances[0],plays));
 */
--- fawler/statement.test.js
+++ fawler/statement.test.js
@@ -1,32 +1,45 @@
 const {statement, amountFor, volumeCreditsFor, usd, totalvolumeCredits,
-  totalAmount
+  totalAmount, playFor
 }
    = require('./statement');
 
-const invoices = require('./invoices.json');
-//  const plays = require('./plays.json');
+//const invoices = require('./invoices.json');
+//const plays = require('./plays.json');
 
 
 test('statement', () => {
+  const invoices = require('./invoices.json');
+  const plays = require('./plays.json');
   let result = 'Statement for BigCo\n';
   result += '  Hamlet: $650.00 (55 seats)\n';
   result += '  As You Like It: $580.00 (35 seats)\n';
   result += '  Othello: $500.00 (40 seats)\n';
   result += 'Amount owed is $1,730.00\n';
   result += 'You earned 47 credits\n';
-  expect(statement(invoices[0])).toBe(result);
+  expect(statement(invoices[0],plays)).toBe(result);
+});
+
+test(`playFor`, ()=> {
+  const invoices = require('./invoices.json');
+  const plays = require('./plays.json');
+  const result = { name: 'Hamlet', type: 'tragedy' };
+  expect(playFor(invoices[0].performances[0], plays)).toStrictEqual(result);
 });
 
 test('amountFor', () => {
-  expect(amountFor(invoices[0].performances[0])).toBe(65000);
-  expect(amountFor(invoices[0].performances[1])).toBe(58000);
-  expect(amountFor(invoices[0].performances[2])).toBe(50000);
+  const invoices = require('./invoices.json');
+  const plays = require('./plays.json');
+  expect(amountFor(invoices[0].performances[0], plays)).toBe(65000);
+  expect(amountFor(invoices[0].performances[1], plays)).toBe(58000);
+  expect(amountFor(invoices[0].performances[2], plays)).toBe(50000);
 });
 
 test(`volumeCreditsFor`, () => {
-  expect(volumeCreditsFor(invoices[0].performances[0])).toBe(25);
-  expect(volumeCreditsFor(invoices[0].performances[1])).toBe(12);
-  expect(volumeCreditsFor(invoices[0].performances[2])).toBe(10);
+  const invoices = require('./invoices.json');
+  const plays = require('./plays.json'); 
+  expect(volumeCreditsFor(invoices[0].performances[0],plays)).toBe(25);
+  expect(volumeCreditsFor(invoices[0].performances[1],plays)).toBe(12);
+  expect(volumeCreditsFor(invoices[0].performances[2],plays)).toBe(10);
 });
 
 test(`usd`, () => {
@@ -38,9 +51,13 @@ test(`usd`, () => {
 });
 
 test('totalVolumeCredits', () => {
-  expect(totalvolumeCredits(invoices[0])).toBe(47);
+  const invoices = require('./invoices.json');
+  const plays = require('./plays.json'); 
+  expect(totalvolumeCredits(invoices[0],plays)).toBe(47);
 });
 
 test('totalAmount', () => {
-  expect(totalAmount(invoices[0])).toBe(173000);
+  const invoices = require('./invoices.json');
+  const plays = require('./plays.json');
+  expect(totalAmount(invoices[0],plays)).toBe(173000);
 });
